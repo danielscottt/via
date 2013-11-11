d73080c63179f06c6cf1c9d07e688cfb592affd4
dan pittman <danielscottt@gmail.com>
2013-11-11 10:01:48 -0800

    redesign.  gonna blog about building via.js while building via.js

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,4 +1,5 @@
 $(function() {
+
   var navOffset = $('#nav-left').offset().top;
   $(window).scroll(function() {
     if ($(this).scrollTop() > navOffset) {
@@ -12,7 +13,15 @@ $(function() {
         'position': 'absolute',
         'margin-top': '0'
       });
-
     }
   });
+
+  $('#nav-left a').on('mouseover', function() {
+    var hoverColor = $('i', this).attr('data-hover-color');
+    $('i', this).animate({color: hoverColor}, 200);
+  });
+  $('#nav-left a').on('mouseout', function() {
+    $('i', this).css('color', '#222');
+  });
+
 });
